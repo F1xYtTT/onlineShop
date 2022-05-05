3b8e99cd9983b12269c89ee5d284f42d26d02f00
F1xYtTT <zero.zero.zero.zero2004@gmail.com>
2022-05-05 20:05:17 +0300

    ADD ACCOUNTS

--- config/settings.py
+++ config/settings.py
@@ -34,14 +34,17 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'games.apps.GamesConfig',
+    'accounts.apps.AccountsConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'games.apps.GamesConfig',
-    'accounts.apps.AccountsConfig',
+    
+    
+
     
 ]
 
@@ -129,4 +132,5 @@ STATIC_URL = 'static/'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 LOGIN_REDIRECT_URL = 'home'
 LOGOUT_REDIRECT_URL = 'home'
+EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
 
